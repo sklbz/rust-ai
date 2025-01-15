36cb0374ace91ea3b4b257b7a7c5bb89ca399052
sklbz <sokol.bozanic@tumo.world>
2025-01-15 09:40:02 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate neuroflow;
+
+use neuroflow::activators::Type::Tanh;
+use neuroflow::data::DataSet;
+use neuroflow::io;
+use neuroflow::FeedForward;
+
 fn main() {
-    println!("Hello, world!");
+    let architecture = vec![3, 5, 5, 2];
+    let mut neural_net = FeedForward::new(&architecture);
+
+    let data = DataSet::from_csv("data/test.csv");
+
+    const ITERATIONS: i64 = 50_000;
+
+    neural_net
+        .activation(Tanh)
+        .learning_rate(0.01)
+        .momentum(0.15)
+        .train(&data, ITERATIONS);
+
+    io::save(&mut neural_net, "models/test.flow").unwrap();
 }

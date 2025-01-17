8472bc4f47bb183b917e6e6d21994333e36e67f2
sklbz <sokol.bozanic@tumo.world>
2025-01-17 17:33:14 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 extern crate neuroflow;
 
-use neuroflow::activators::Type::Tanh;
+// use neuroflow::activators::Type::Tanh;
 use neuroflow::data::DataSet;
-use neuroflow::io;
-use neuroflow::FeedForward;
+// use neuroflow::io;
+// use neuroflow::FeedForward;
 
 fn main() {
-    let architecture = vec![3, 5, 5, 2];
-    let mut neural_net = FeedForward::new(&architecture);
+    // let architecture = vec![3, 5, 5, 2];
+    // let mut neural_net = FeedForward::new(&architecture);
 
-    let mut data = DataSet::from_csv("data/test.csv");
+    let data = DataSet::from_csv("data/test.csv");
+    println!("{:#?}", &data);
 
-    const ITERATIONS: i64 = 50_000;
-    neural_net
-        .activation(Tanh)
-        .learning_rate(0.1)
-        .momentum(0.15)
-        .train(&data, ITERATIONS);
-
-    io::save(&mut neural_net, "models/test.flow").unwrap();
+    /*
+     * const ITERATIONS: i64 = 50_000;
+     * neural_net.activation(Tanh)
+     *      .learning_rate(0.1)
+     *      .momentum(0.15)
+     *      .train(&data, ITERATIONS);
+     *
+     * io::save(&mut neural_net, "models/test.flow").unwrap();
+     */
 }

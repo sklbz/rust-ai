c3d7ac3fa620d5f2c3c72284cbd2c687cce7bdf9
sklbz <sokol.bozanic@tumo.world>
2025-01-18 17:07:59 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod train;
+
 extern crate neuroflow;
 
 use neuroflow::activators::Type::Tanh;
@@ -5,18 +7,4 @@ use neuroflow::data::DataSet;
 use neuroflow::io;
 use neuroflow::FeedForward;
 
-fn main() {
-    let architecture = vec![3, 5, 2];
-    let mut neural_net = FeedForward::new(&architecture);
-
-    let data: DataSet = DataSet::from_csv("data/test.csv").expect("Unable to load data");
-
-    const ITERATIONS: i64 = 50_000;
-    neural_net
-        .activation(Tanh)
-        .learning_rate(0.1)
-        .momentum(0.15)
-        .train(&data, ITERATIONS);
-
-    io::save(&mut neural_net, "models/test.flow").expect("Unable to save model");
-}
+fn main() {}

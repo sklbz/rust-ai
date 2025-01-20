6d7157da577add74e30ccba92ecddc60777113e7
sklbz <sokol.bozanic@tumo.world>
2025-01-20 19:09:56 +0100

    update

--- src/test.rs
+++ src/test.rs
@@ -3,7 +3,9 @@ use neuroflow::{io, FeedForward};
 pub fn test() -> bool {
     let mut neural_net: FeedForward = io::load("models/test.flow").expect("Failed to load model");
 
-    let input = [1.0, 3.0, 15.0];
+    let input = [11.0, 13.0, 15.0];
+    // [1.0, 3.0, 15.0] -> [1, 5]
+    // [11.0, 13.0, 15.0] -> [5, 8]
 
     let output = neural_net.calc(&input);
 

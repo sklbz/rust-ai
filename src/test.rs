7a45c3533e028b5155134214edadf0d0acc09892
sklbz <sokol.bozanic@tumo.world>
2025-01-20 19:05:56 +0100

    as expected, the ai behave weirdly with large values

--- src/test.rs
+++ src/test.rs
@@ -3,7 +3,7 @@ use neuroflow::{io, FeedForward};
 pub fn test() -> bool {
     let mut neural_net: FeedForward = io::load("models/test.flow").expect("Failed to load model");
 
-    let input = [1.0, 3.0, 5.0];
+    let input = [1.0, 3.0, 15.0];
 
     let output = neural_net.calc(&input);
 

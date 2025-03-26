1f6a63a4502725b8a46afec03a9f35c0d306aef7
sklbz <122096315+sklbz@users.noreply.github.com>
2025-03-26 15:46:02 +0100

    Update mlp.rs

--- src/mlp.rs
+++ src/mlp.rs
@@ -1,4 +1,9 @@
+use nalgebra::Matrix;
 enum MultiLayerPerceptron {
   architecture: &Vec<u32>,
-  
+  // Should I reduce all the operations to a single matrix multiplication or not ?
+  // Does adding more layers really makes a model more efficient ? 
+  // A layer can be represented as a matrix A
+  // But A'A (the succession of the two layers) is also a matrix
+  // Maybe it is just about having enoug weights to parametrize ?
 }

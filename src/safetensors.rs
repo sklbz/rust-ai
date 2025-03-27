f327d4bb735b1f12f44b34f791cdf2cfbd68293a
sklbz <sokol.bozanic@tumo.world>
2025-03-27 16:16:33 +0100

    fixed some more bugs

--- src/safetensors.rs
+++ src/safetensors.rs
@@ -1,5 +1,6 @@
 use neuroflow::{io, FeedForward};
 
+#[allow(dead_code)]
 pub fn load() -> () {
     let mut model = io::load::<FeedForward>("models/model-00001-of-000163.safetensors")
         .expect("Failed to load tensor");
